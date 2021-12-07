4e2697db50adbadf22ece9e56b107193f5927b2f
DanielMagallon <danielmagallon123@gmail.com>
2021-12-07 02:04:04 -0600

    DP API

--- src/Manager.py
+++ src/Manager.py
@@ -23,20 +23,64 @@ except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
 
 
+
+def wrapper_custom_query(*args,**kwargs):
+
+    def decorator(func):
+        def wrapper(*wargs,**wkwargs):
+            data_func = func(*wargs,**wkwargs)
+            args2 = ['{}.{}'.format(kwargs["table_name"],a) for a in args]
+            fields = str(args2).replace("[","").replace("]","").replace("'","")
+
+            if kwargs.get('selectjoin'):
+                select_args = str(kwargs['selectjoin']).replace("[","").replace("]","").replace("'","")
+                select = f"SELECT {fields},{select_args} FROM {kwargs['table_name']}"
+            else:
+                select = f"SELECT {fields} FROM {kwargs['table_name']}"
+
+            if kwargs.get('innerjoin') and kwargs.get('on'):
+                onq = kwargs['on']
+                index=0
+                joinquery=''
+                for injoin in kwargs['innerjoin']:
+                    on1 = onq[index]
+                    index+=1
+                    on2 = onq[index]
+                    index+=1
+                    joinquery+=f' inner join {injoin} on {on1}={on2}'
+
+                query = select + joinquery
+            else:
+                query = select
+
+            if data_func.get('where'):
+                query += data_func['where']
+
+            print(f'Custo query: {query}')
+            cursor.execute(query)
+            record = cursor.fetchall()
+
+            return json.dumps({'fields': data_func['tables_fields'], 'values': record})
+
+
+        return wrapper
+
+    return decorator
+
+
 def wrapper_query_all(*args,**kwargs):
 
     def decorator(func):
-        def wrapper():
-            # data: dict = func()
+        def wrapper(*wargs,**wkwargs):
+            data_func: dict = func(*wargs,**wkwargs)
             query = f'SELECT * FROM {kwargs["table_name"]}'
             print(f"Query: {query}")
             cursor.execute(query)
             record = cursor.fetchall()
 
-            dic = {'fields':args,'values': record}
+            dic = {'fields':data_func['tables_fields'],'values': record}
 
-            print(f"Record dic: {dic}")
-            return json.dumps(dic)
+            return dic
 
         return wrapper
 
@@ -48,23 +92,22 @@ def wrapper_insert(*args,**kwargs):
     def decorator(function):
         def wrapper(*arg2,**kwargs2):
             data: dict = function(*arg2,**kwargs2)
-            print(f"Data: {data}")
+
             fields = str(list(data['field_values'].keys())).replace("[","(").replace("]",")").replace("'","")
             values = str(list(data['field_values'].values())).replace("[","(").replace("]",")")
             query = f'INSERT INTO {kwargs["table_name"]} {fields} VALUES {values}'
 
-            print(f"Query: {query}")
             try:
                 cursor.execute(query)
-                dic = {"status": 0,"title": f"Tabla '{kwargs['table_name']}' actualizada",
-                       "msg": "Registro ingresado correctamente"}
+                dic = {'status': 0,'title': f'Tabla "{kwargs["table_name"]}" actualizada',
+                       'msg': 'Registro ingresado correctamente'}
 
             except Exception as ex:
-                dic = {"status": -1, "title": f"Error al registar en  {kwargs['table_name']}",
-                        "msg": repr(ex)}
+                dic = {'status': -1, 'title': f'Error al registar en  {kwargs["table_name"]}',
+                        'msg': repr(ex)}
 
             connection.commit()
-            return json.dumps(dic)
+            return dic
 
         return wrapper
 
@@ -75,13 +118,14 @@ def wrapper_insert(*args,**kwargs):
 
 
 @wrapper_insert(table_name='grupos')
-def insert_grupo(nombre):
-    return { 'field_values': {'nombre': nombre} }
+def insert_grupo(nombre,cicloescolar,nrocontrol_tutor,idcarrera):
+    return { 'field_values': {'nombre': nombre,'nrocontrol_tutor':nrocontrol_tutor,
+                              'cicloescolar':cicloescolar,'idcarrera':idcarrera} }
 
 
-@wrapper_query_all("idgrupo","nombre", table_name='grupos')
+@wrapper_query_all(table_name='grupos')
 def consulta_general_grupos():
-    ...
+    return {'tables_fields':["Id Grupo","Nombre","Nro Control tutor","Ciclo Escolar", "Id carrera","Activo"]}
 
 
 '''Fin seccin grupos'''
@@ -96,9 +140,9 @@ def insert_alumno(nrocontrol,nombre,apellido1,apellido2,idgrupo):
                               'nrocontrol': nrocontrol}}
 
 
-@wrapper_query_all("nombre", "apellido1", "apellido2", "idgrupo", "nrocontrol", table_name='alumnos')
+@wrapper_query_all(table_name='alumnos')
 def consulta_general_alumnos():
-    ...
+    return {'tables_fields':["Alumno","Apellido 1","Apellido 2","Nro Control"]}
 
 
 '''FIn seccion alumnos'''
@@ -113,10 +157,15 @@ def insert_tutor(nrocontrol,nombre,apellido1,apellido2,idcarrera):
                               'apellido2':apellido2,'idcarrera':idcarrera}}
 
 
-@wrapper_query_all("nrocontrol", "nombre", "apellido1", "apellido2", "idcarrera",table_name='tutores')
-def consulta_general_tutor():
-    ...
+# @wrapper_query_all("nrocontrol", "nombre", "apellido1", "apellido2", "idcarrera",table_name='tutores')
+# def consulta_general_tutor():
+#     ...
 
+
+@wrapper_custom_query("nrocontrol","nombre","apellido1","apellido2",table_name="tutores",
+                      selectjoin=["carreras.nombre"],innerjoin=["carreras"],on=["tutores.idcarrera","carreras.idcarrera"])
+def consulta_tutores():
+    return {'tables_fields':["Nro Control","Tutor","Apellido 1","Apellido 2","Carrera"]}
 
 '''Fin seccion tutores'''
 
@@ -129,9 +178,9 @@ def insert_carrera(nombre,tipo,total_creditos):
     return { 'field_values': {'nombre': nombre,'tipo':tipo,'total_creditos':total_creditos}}
 
 
-@wrapper_query_all("nombre", "tipo","total_creditos",table_name='carreras')
+@wrapper_query_all(table_name='carreras')
 def consulta_general_carrera():
-    ...
+    return {'tables_fields':["Carrera","Tipo","Total Creditos"]}
 
 
 '''Fin seccion carreas'''
@@ -145,9 +194,9 @@ def insert_materia(codigo_materia,nombre,creditos):
     return { 'field_values': {'codigo_materia':codigo_materia,'nombre': nombre,'creditos':creditos}}
 
 
-@wrapper_query_all("codigo_materia","nombre", "creditos",table_name='materias')
+@wrapper_query_all(table_name='materias')
 def consulta_general_materia():
-    ...
+    return {'tables_fields':["Codigo ","Materia","Creditos"]}
 
 
 '''Fin seccion materias'''
@@ -161,29 +210,36 @@ def insert_alumno_materia(codigo_materia,nrocontrol):
     return { 'field_values': {'nrocontrol':nrocontrol,'codigo_materia': codigo_materia}}
 
 
-@wrapper_query_all("nrocontrol","codigo_materia", "calificacion","oportunidad","asesoria",table_name='alumnos_materias')
-def consulta_general_alumno_materia():
-    ...
+# @wrapper_query_all("nrocontrol","codigo_materia", "calificacion","oportunidad","asesoria",table_name='alumnos_materias')
+# def consulta_general_alumno_materia():
+#     ...
 
 
 '''Fin seccion alumnos_materias'''
 
 
-'''Seccion grupo_tutores'''
+'''Seccion Pagina Consulta/Altas Tutores'''
 
 
-@wrapper_insert(table_name='grupo_tutores')
-def insert_grupo_docente(idgrupo,nrocontrol,cicloescolar):
-    return { 'field_values': {'nrocontrol':nrocontrol,'idgrupo': idgrupo,'cicloescolar':cicloescolar}}
+@wrapper_custom_query("nrocontrol","nombre","apellido1","apellido2",table_name="tutores",
+                      selectjoin=["carreras.nombre"],innerjoin=["carreras"],on=["tutores.idcarrera","carreras.idcarrera"])
+def consulta_tutores():
+    return {'tables_fields':["Nro Control","Tutor","Apellido 1","Apellido 2","Carrera"]}
 
 
-@wrapper_query_all("nrocontrol","idgrupo", "cicloescolar",table_name='grupo_tutores')
-def consulta_general_grupo_docente():
-    ...
+@wrapper_custom_query("idgrupo","nombre","cicloescolar","idcarrera",table_name="grupos")
+def __extraer_grupo_tutor(nrocontrol_tutor):
+    return {'where': f"nrocontrol_tutor='{nrocontrol_tutor}' and activo",
+            'table_fields':["Id grupo","Nombre","Ciclo escolar","Id carrera"]}
 
 
-'''Fin seccion grupo_tutores'''
+def consulta_pagina_tutores(nrocontrol_tutor):
+    paso1 = __extraer_grupo_tutor(nrocontrol_tutor)
+    print(f"Paso 1: Grupo = {paso1}")
+    return "Ok"
 
+
+'''Fin Seccion'''
 
 def close():
     if connection:
--- src/Server.py
+++ src/Server.py
@@ -2,53 +2,64 @@ import json
 
 from src import *
 from flask_cors import cross_origin
-from flask import Response,request
+from flask import Response, request
+
 '''
 Seccion Grupos
 '''
 
 
-@app.route('/grupos/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/grupos/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def grupos_consulta_all():
     return Response(consulta_general_grupos())
 
 
-@app.route('/grupos/registro', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/grupos/registro', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def grupos_registro():
     data = request.get_json()
     nombre = data['nombre']
-    return Response(insert_grupo(nombre))
+    nrocontrol_tutor: data['nrocontrol_tutor']
+    cicloescolar: data['cicloescolar']
+    idcarrera: data['idcarrera']
+    return Response(insert_grupo(nombre=nombre, nrocontrol_tutor=nrocontrol_tutor, cicloescolar=cicloescolar,
+                                 idcarrera=idcarrera))
 
 
-@app.route('/grupos/multiregistro', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/grupos/multiregistro', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def grupos_multiregistro():
     data = request.get_json()
-    promises = {}
-    index = 0
+    promises = {"ok": [], "error": []}
+
     for json_row in data:
         nombre = json_row['nombre']
-        promises[index]=insert_grupo(nombre)
-        index+=1
+        nrocontrol_tutor = json_row['nrocontrol_tutor']
+        cicloescolar = json_row['cicloescolar']
+        idcarrera = json_row['idcarrera']
+
+        result = insert_grupo(nombre=nombre, nrocontrol_tutor=nrocontrol_tutor, cicloescolar=cicloescolar,
+                              idcarrera=idcarrera)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
+
 
 '''
 Fin Seccion Grupos
 '''
-
 
 '''
 Seccion Tutores
 '''
 
 
-@app.route('/tutores/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/tutores/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def tutores_consulta_all():
-    return Response(consulta_general_tutor())
+    return Response(consulta_tutores())
 
 
 @app.route("/tutores/registro", methods=["POST"])
@@ -60,25 +71,28 @@ def tutores_registro():
     idcarrera = data['idcarrera']
     nrocontrol = data['nrocontrol']
     return Response(insert_tutor(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
-                        nrocontrol=nrocontrol,idcarrera=idcarrera))
+                                 nrocontrol=nrocontrol, idcarrera=idcarrera))
 
 
 @app.route("/tutores/multiregistro", methods=["POST"])
 def tutores_multiregistro():
     data = request.get_json()
-    promises = {}
-    index = 0
+    promises = {"ok":[],"error":[]}
+
     for json_row in data:
         nombre = json_row['nombre']
         apellido1 = json_row['apellido1']
         apellido2 = json_row['apellido2']
         idcarrera = json_row['idcarrera']
         nrocontrol = json_row['nrocontrol']
-        promises[index]=insert_tutor(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
-                        nrocontrol=nrocontrol,idcarrera=idcarrera)
-        index+=1
+
+        result = insert_tutor(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
+                                       nrocontrol=nrocontrol, idcarrera=idcarrera)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
+
 
 '''
 Fin Seccion Tutores
@@ -89,8 +103,8 @@ Seccion alumnos
 '''
 
 
-@app.route('/alumnos/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/alumnos/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def alumnos_consulta_all():
     return Response(consulta_general_alumnos())
 
@@ -103,24 +117,26 @@ def alumnos_registro():
     apellido2 = data['apellido2']
     idgrupo = data['idgrupo']
     nrocontrol = data['nrocontrol']
-    return Response(insert_alumno(nombre=nombre, apellido1=apellido1,apellido2=apellido2,
-                         idgrupo=idgrupo, nrocontrol=nrocontrol))
+    return Response(insert_alumno(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
+                                  idgrupo=idgrupo, nrocontrol=nrocontrol))
 
 
 @app.route('/alumnos/multiregistro', methods=['POST'])
 def alumnos_multiregistro():
     data = request.get_json()
-    promises = {}
-    index = 0
+    promises = {"ok":[],"error":[]}
+
     for json_row in data:
         nombre = json_row['nombre']
         apellido1 = json_row['apellido1']
         apellido2 = json_row['apellido2']
         idgrupo = json_row['idgrupo']
         nrocontrol = json_row['nrocontrol']
-        promises[index]=insert_alumno(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
-                      idgrupo=idgrupo, nrocontrol=nrocontrol)
-        index+=1
+
+        result = insert_alumno(nombre=nombre, apellido1=apellido1, apellido2=apellido2,
+                                        idgrupo=idgrupo, nrocontrol=nrocontrol)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
 
@@ -129,13 +145,13 @@ def alumnos_multiregistro():
 Fin Seccion alumnos
 '''
 
-
 '''
 Seccion carreras
 '''
 
-@app.route('/carreras/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+
+@app.route('/carreras/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def carreras_consulta_all():
     return Response(consulta_general_carrera())
 
@@ -145,20 +161,22 @@ def carreras_registro():
     data = request.get_json()
     nombre = data['nombre']
     tipo = data['tipo']
-    return Response(insert_carrera(nombre=nombre,tipo=tipo))
+    return Response(insert_carrera(nombre=nombre, tipo=tipo))
 
 
 @app.route('/carreras/multiregistro', methods=['POST'])
 def carreras_multiregistro():
     data = request.get_json()
-    promises = {}
-    index = 0
+    promises = {"ok":[],"error":[]}
+
     for json_row in data:
         nombre = json_row['nombre']
         tipo = json_row['tipo']
         total_creditos = json_row['total_creditos']
-        promises[index]=insert_carrera(nombre=nombre, tipo=tipo,total_creditos=total_creditos)
-        index+=1
+
+        result = insert_carrera(nombre=nombre, tipo=tipo, total_creditos=total_creditos)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
 
@@ -167,14 +185,13 @@ def carreras_multiregistro():
 Fin seccion carreas
 '''
 
-
 '''
 Seccion materias
 '''
 
 
-@app.route('/materias/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/materias/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def materias_consulta_all():
     return Response(consulta_general_materia())
 
@@ -185,35 +202,37 @@ def materias_registro():
     codigo_materia = data['codigo_materia']
     nombre = data['nombre']
     creditos = data['creditos']
-    return Response(insert_materia(nombre=nombre,codigo_materia=codigo_materia,creditos=creditos))
+    return Response(insert_materia(nombre=nombre, codigo_materia=codigo_materia, creditos=creditos))
 
 
 @app.route('/materias/multiregistro', methods=['POST'])
 def materias_multiregistro():
     data = request.get_json()
-    promises = {}
-    index=0
+    promises = {"ok":[],"error":[]}
+
     for json_row in data:
         codigo_materia = json_row['codigo_materia']
         nombre = json_row['nombre']
         creditos = json_row['creditos']
-        promises[index] = insert_materia(nombre=nombre,codigo_materia=codigo_materia,creditos=creditos)
-        index+=1
+
+        result = insert_materia(nombre=nombre, codigo_materia=codigo_materia, creditos=creditos)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
+
 
 '''
 Fin seccion materias
 '''
-
 
 '''
 Seccion alumno-materias
 '''
 
 
-@app.route('/alumno-materias/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
+@app.route('/alumno-materias/consulta/all', methods=['POST', 'GET'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def materia_alumnos_consulta_all():
     return Response(consulta_general_alumno_materia())
 
@@ -223,65 +242,27 @@ def materias_alumnos_registro():
     data = request.get_json()
     codigo_materia = data['codigo_materia']
     nrocontrol = data['nrocontrol']
-    return Response(insert_alumno_materia(codigo_materia=codigo_materia,nrocontrol=nrocontrol))
+    return Response(insert_alumno_materia(codigo_materia=codigo_materia, nrocontrol=nrocontrol))
 
 
-@app.route('/alumno-materias/multiregistro', methods=['POST'])
+@app.route('/alumnos_materias/multiregistro', methods=['POST'])
 def materias_alumnos_multiregistro():
     data = request.get_json()
-    promises = {}
-    index = 0
+    promises = {"ok": [], "error": []}
+
     for json_row in data:
         codigo_materia = json_row['codigo_materia']
         nrocontrol = json_row['nrocontrol']
-        promises[index]=insert_alumno_materia(codigo_materia=codigo_materia, nrocontrol=nrocontrol)
-        index+=1
+
+        result = insert_alumno_materia(codigo_materia=codigo_materia, nrocontrol=nrocontrol)
+
+        promises["ok" if result['status'] == 0 else "error"].append(result)
 
     return Response(json.dumps(promises))
 
 
 '''
 Fin seccion alumno-materias
-'''
-
-
-'''
-Seccion grupo_tutores
-'''
-
-
-@app.route('/grupo-tutores/consulta/all', methods=['POST','GET'])
-@cross_origin(origin='*',headers=['Content-Type'])
-def grupo_tutores_consulta_all():
-    return Response(consulta_general_grupo_docente())
-
-
-@app.route('/grupo-tutores/registro', methods=['POST'])
-def grupo_tutores_registro():
-    data = request.get_json()
-    idgrupo = data['idgrupo']
-    nrocontrol = data['nrocontrol']
-    cicloescolar = data['cicloescolar']
-    return Response(insert_grupo_docente(idgrupo=idgrupo,nrocontrol=nrocontrol,cicloescolar=cicloescolar))
-
-
-@app.route('/grupo-tutores/multiregistro', methods=['POST'])
-def grupo_tutores_multiregistro():
-    data = request.get_json()
-    promises = {}
-    index = 0
-    for json_row in data:
-        idgrupo = json_row['idgrupo']
-        nrocontrol = json_row['nrocontrol']
-        cicloescolar = json_row['cicloescolar']
-        promises[index] = insert_grupo_docente(idgrupo=idgrupo, nrocontrol=nrocontrol, cicloescolar=cicloescolar)
-        index+=1
-
-    return Response(json.dumps(promises))
-
-
-'''
-Fin seccion grupo-tutores
 '''
 
 if __name__ == '__main__':
